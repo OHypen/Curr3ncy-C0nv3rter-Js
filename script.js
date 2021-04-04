7eca6def18a9df960914d996268d25ab16a509c4
OLuisH <goldff36@gmail.com>
2021-04-04 00:11:35 -0300

    Iniciando o projeto

---
+++ script.js
@@ -0,0 +1,23 @@
+// let nome = "OLuisH"
+ // alert(nome)
+
+// let idade = 12
+ // alert(idade)
+
+// let idadeSoma = 12
+   // idadeSoma = idadeSoma + 1
+// alert(idadeSoma)
+
+// let idadeUsuario = prompt("Insira sua idade:")
+// let idadeUsuarioSoma = parseInt(idadeUsuario)
+   // idadeUsuarioSoma = idadeUsuarioSoma + 1
+// alert(idadeUsuarioSoma)
+
+
+
+let valorDolarText = prompt("Valor em dólar:")
+ let valorDolarNumber = parseFloat(valorDolarText)
+
+let valorReal = valorDolarNumber * 5.50
+let valorRealFixed = valorReal.toFixed(2)
+alert(valorRealFixed)
