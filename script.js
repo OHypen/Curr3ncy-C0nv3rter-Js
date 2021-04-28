966fa767f2c50f3e6e591035581afa84c97183f9
Luis H <goldff36@gmail.com>
2021-04-28 14:44:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// let nome = "OLuisH"
- // alert(nome)
-
-// let idade = 12
- // alert(idade)
-
-// let idadeSoma = 12
-   // idadeSoma = idadeSoma + 1
-// alert(idadeSoma)
-
-// let idadeUsuario = prompt("Insira sua idade:")
-// let idadeUsuarioSoma = parseInt(idadeUsuario)
-   // idadeUsuarioSoma = idadeUsuarioSoma + 1
-// alert(idadeUsuarioSoma)
-
 
 
 let valorDolarText = prompt("Valor em dólar:")
